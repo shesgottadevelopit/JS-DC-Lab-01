3d6fdef37e5f3e28439a14c1b5d8a4439ce8f17a
a.chigozie <shesgottadevelopit@gmail.com>
2017-06-01 00:28:17 -0400

    hmm

--- classes.js
+++ classes.js
@@ -3,25 +3,20 @@
 const suits = ['hearts', 'clubs', 'spades', 'diamonds']
 const ranks = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
 const rankScores = { ace: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10, jack: 11, king: 12, queen: 13 }
+// const suits = ['hearts', 'clubs', 'spades']
+// const ranks = ['ace', 'two', 'three']
+// const rankScores = { ace: 1, two: 2, three: 3}
 
+
+// CLASSES
 
 // class for an individual card
-/*  EXAMPLE
-    {
-      suit: 'hearts',
-      rank: 'ace',
-      title: 'ace of hearts',
-      score: 1
-    }
-*/
-
 class Card {
     constructor(rank, suit) {
         this.suit = suit;
         this.rank = rank;
         this.title = this.rank + ' of ' + this.suit;
-        //this.score = rankScores[rank]
-        this.score = rankScores[this.rank]
+        this.score = rankScores[this.rank]; // or //this.score = rankScores[rank]
     }
 }
 
@@ -37,7 +32,6 @@ class DeckofCards {
             if (this.cards.length === 0) {
                 for(let i = 0; i < ranks.length; i ++) {
                     for (let j = 0; j < suits.length; j++) {
-                        //this.cards.push(ranks[i] + ' of ' + suits[j])
                         this.cards.push(new Card(ranks[i], suits[j]))
                     }
                 }
@@ -45,26 +39,54 @@ class DeckofCards {
                 return;
             }
 
-        }
+        };
+        this.removeCard = function(position) {
+            this.cards.splice(position,1);
+            return position;
+        };
+        this.getRandomCardToShuffle = function() {
+            let randomCard = Math.floor(Math.random() * this.cards.length);
+            return randomCard;
+
+
+        };
+        this.shuffleCards = function(x) {
+            for (let i = 0; i <= x; i++) {
+                this.cards.push(this.getRandomCardToShuffle(this.cards[i]));
+
+            }
+        };
     }
-
 }
-
-let dealNewDeck = new DeckofCards(ranks, suits);
-const newDeck = new DeckofCards(ranks, suits);
-newDeck.createNewDeck(ranks, suits);
-let handOfCards = newDeck.cards;
-
-
 // class for players
-
 class Player {
     constructor(username='Program Generated Player') {
         this.username = username;
         //this.hand = handOfCards;
         this.hand = [];
+        newDeck.dealHand(this.hand);
     }
 }
-let Queen = new Player('Queen')
-console.log(Queen);
-console.log(new Player());
+
+
+// assign card deck to a variable and create a deck
+const newDeck = new DeckofCards(ranks, suits);
+newDeck.createNewDeck(ranks, suits);
+//console.log(newDeck.cards.length)
+//console.log(newDeck.cards)
+
+
+// test shuffled deck
+newDeck.shuffleCards();
+console.log(newDeck.cards.length)
+console.log(newDeck.cards)
+
+
+
+// let Queen = new Player('Queen');
+// let King = new Player('King');
+// console.log(Queen);
+// console.log(King);
+// console.log(Queen.hand.length)
+// console.log(King.hand.length);
+// console.log(newDeck.cards.length)
