d8aba2eae37a7fd10854e97d013fa49212242cfb
a.chigozie <shesgottadevelopit@gmail.com>
2017-06-01 01:54:07 -0400

    figuring out what needs to be added to the game class and if a game class is even needed

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
 function newGame() {
 
 }
+// class for a game
+class Game {
+    constructor() {
+        this.gameStats = {
+            currentRound: function() {
+                // figure out a way to count the number of times both players pulled a card
+            },
+            printPlayerStats: x
+        }
+        this.wonCurrentRound = '';
+        this.wonTheGame = '';
+
+    }
+}
+
+let letsPlay = new Game();
